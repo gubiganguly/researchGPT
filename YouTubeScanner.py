ec60987c47236dade83b92eaf98c09ea4109b13d
Gubi Ganguly <gubiisepic@Gubis-MacBook-Pro.local>
2024-01-05 13:04:11 -0500

    username possible

--- YouTubeScanner.py
+++ YouTubeScanner.py
@@ -1,13 +1,25 @@
 from googleapiclient.discovery import build
 from youtube_transcript_api import YouTubeTranscriptApi
+import requests
 
 class YouTubeScanner:
-    def __init__(self, api_key, channel_id):
+    def __init__(self, api_key, username = None, channel_id = None):
         self.api_key = api_key
-        self.channel_id = channel_id
         self.youtube = build('youtube', 'v3', developerKey=self.api_key)
         self.latest_video_id = None
+        if username:
+            self.channel_id = self.get_channel_id(username)
+        else:
+            self.channel_id = channel_id
 
+    def get_channel_id(self, username):
+        try:
+            request = self.youtube.search().list(q=username, type='channel', part='id', maxResults=1)
+            response = request.execute()
+            channel_id = response['items'][0]['id']['channelId']
+            return channel_id
+        except Exception as e:
+            return None
 
     # Fetch the transcript of the video
     def fetch_transcript(self, video_id):
@@ -27,37 +39,40 @@ class YouTubeScanner:
     # Fetch latest video id
     def fetch_latest_video(self):
         # Request to get the latest video from the channel
-        request = self.youtube.search().list(
-            part="snippet",
-            channelId=self.channel_id,
-            order="date",  # ensures the latest videos come first
-            maxResults=1  # we only want the latest video
-        )
-        response = request.execute()
-
-        # Extract video ID from the response
-        if response['items']:
-            # Define latest_video_id here from the response
-            latest_video_id = response['items'][0]['id']['videoId']
-            if self.latest_video_id != latest_video_id:
-                self.latest_video_id = latest_video_id
-
-                # Fetch additional details
-                transcript = self.fetch_transcript(latest_video_id)
-
-                # Constructing the JSON data
-                video_data = {
-                    "channel_id": self.channel_id,
-                    "channel_title": response['items'][0]['snippet']['channelTitle'] if response['items'] else 'No title found',
-                    "video_title": response['items'][0]['snippet']['title'] if response['items'] else 'No title found',
-                    "video_id": latest_video_id,
-                    "transcript": transcript
-                }
-
-                return video_data
-            else:
-                print("No new videos since the last check.")
+        if (self.channel_id == None):
+            return None
         else:
-            print("No videos found for the channel.")
-        return None
+            request = self.youtube.search().list(
+                part="snippet",
+                channelId=self.channel_id,
+                order="date",  # ensures the latest videos come first
+                maxResults=1  # we only want the latest video
+            )
+            response = request.execute()
+
+            # Extract video ID from the response
+            if response['items']:
+                # Define latest_video_id here from the response
+                latest_video_id = response['items'][0]['id']['videoId']
+                if self.latest_video_id != latest_video_id:
+                    self.latest_video_id = latest_video_id
+
+                    # Fetch additional details
+                    transcript = self.fetch_transcript(latest_video_id)
+
+                    # Constructing the JSON data
+                    video_data = {
+                        "channel_id": self.channel_id,
+                        "channel_title": response['items'][0]['snippet']['channelTitle'] if response['items'] else 'No title found',
+                        "video_title": response['items'][0]['snippet']['title'] if response['items'] else 'No title found',
+                        "video_id": latest_video_id,
+                        "transcript": transcript
+                    }
+
+                    return video_data
+                else:
+                    print("No new videos since the last check.")
+            else:
+                print("No videos found for the channel.")
+            return None
 
--- app.py
+++ app.py
@@ -1,23 +1,22 @@
 import time
+import os
 from YouTubeScanner import YouTubeScanner  # Make sure this is the name of your file containing the class
 from GPTSummarizer import GPTSummarizer
-
-# Replace with your actual YouTube API Key and desired Channel ID
-YouTube_API_KEY = 'AIzaSyAH73d14VW5PisEXd0j_sOblNvuscrUC2Q'
-Matt_Wolfe_CHANNEL_ID = 'UChpleBmo18P08aKCIgti38g'
-OpenAI_API_KEY = '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'
+from dotenv import load_dotenv
+load_dotenv()
 
 def main():
-    Matt_Wolfe = YouTubeScanner(YouTube_API_KEY , Matt_Wolfe_CHANNEL_ID)
+    Matt_Wolfe = YouTubeScanner(os.getenv("YOUTUBE_API_KEY") , 'mrewolf')
 
     while True:
         new_video_info = Matt_Wolfe.fetch_latest_video()
+        #print(new_video_info)
         if new_video_info:
-            # print(new_video_info)
-            summarizer = GPTSummarizer(OpenAI_API_KEY, new_video_info['transcript'])
+            summarizer = GPTSummarizer(os.getenv("OPENAI_API_KEY"), new_video_info['transcript'])
             summary = summarizer.summarize("Summarize the following youtube transcript into short easy to understand bullet points. The bullet points will be used to write a news letter so keep that in mind, no extra fluff. Here is the transcript:")
             print(new_video_info['channel_title'], " - ", new_video_info['video_title'], "\n", summary)
-
+        else:
+            print("Could not find youtuber id")
         time.sleep(3600)  # Wait for 1 hour before checking again. Adjust as needed.
 
 if __name__ == "__main__":
